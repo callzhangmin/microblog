214730eb999340eac61670e804681c9f44e65681
zhangmin <minzhang@juban.com>
2019-07-02 10:42:01 +0800

    新增用户注册

--- app/forms.py
+++ app/forms.py
@@ -2,11 +2,30 @@
 # -*- coding: utf-8 -*-
 # Created by zhangmin on 2019/6/28 17:15
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextField
+from wtforms.validators import DataRequired, ValidationError, Email, EqualTo
+from app.models import User
 
 class LoginForm(FlaskForm):
     username = StringField(label=u'用户名', validators=[DataRequired()])
     password = PasswordField(label=u'密码',validators=[DataRequired()])
     remember_me = BooleanField(label=u'忘记密码')
     submit = SubmitField(label=u'登录')
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username',validators=[DataRequired()])
+    email = StringField('Email',validators=[DataRequired(),Email()])
+    password = PasswordField('password',[DataRequired()])
+    password2 = PasswordField('Repeat Password',validators=[DataRequired(),EqualTo('password')])
+    submit = SubmitField('提交注册')
+
+    def validate_username(self,username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError("用户名已存在！")
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('该邮箱已被用户注册！')
--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # Created by zhangmin on 2019/6/28 14:37
-from app import app
+from app import app,db
 from flask import render_template, flash, redirect, url_for, request
-from app.forms import LoginForm
+from app.forms import LoginForm,RegistrationForm
 from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User
 from werkzeug.urls import url_parse
@@ -45,7 +45,22 @@ def login():
 
 
 
-@app.route('/login')
+@app.route('/logout')
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+
+@app.route('/register',methods=['GET','POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data,email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('注册成功！')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
